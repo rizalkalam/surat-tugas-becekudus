34fe02c9422fdee263b97256407beee08090baaa
Rizal Kalam <89380544+rizalkalam@users.noreply.github.com>
2024-02-12 10:57:12 +0700

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 // api/apiurl.js
-// const apiurl = "https://gorgeous-boa-smiling.ngrok-free.app/api/";
-const apiurl = "https://bonefish-humane-raptor.ngrok-free.app/api/";
+const apiurl = "https://gorgeous-boa-smiling.ngrok-free.app/api/";
+// const apiurl = "https://bonefish-humane-raptor.ngrok-free.app/api/";
 
 export default apiurl;
